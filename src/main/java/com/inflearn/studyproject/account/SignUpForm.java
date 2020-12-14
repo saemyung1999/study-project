3c516b763dace2794a3a4c45b1ed3b57b44b3355
saemyung1999 <saemyung1999@gmail.com>
2020-12-15 00:16:47 +0900

    회원가입 - 1.화면단 코드 작성 2.간단한 컨트롤러 코드 작성 3.테스트

---
+++ src/main/java/com/inflearn/studyproject/account/SignUpForm.java
@@ -0,0 +1,13 @@
+package com.inflearn.studyproject.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+}
---
+++ src/main/java/com/inflearn/studyproject/domain/Account.java
@@ -0,0 +1,53 @@
+package com.inflearn.studyproject.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Builder @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Entity
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER) //일반적으로 String은 DB에서 VARCHAR타입으로 매칭되는데 이미지의 데이터 크기는 더 커질 수 있으므로 @Lob을 이용하여 텍스트 타입으로 매핑해줌
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb;
+}
