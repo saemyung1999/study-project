2f565356c35ff31070f08b48d8ab67c92b09fd2e
saemyung1999 <saemyung1999@gmail.com>
2020-12-15 16:27:59 +0900

    회원가입 - 1. @Valid, Custom Validator를 이용한 사용자 정보 검증

--- src/main/java/com/inflearn/studyproject/account/AccountController.java
+++ src/main/java/com/inflearn/studyproject/account/AccountController.java
@@ -1,15 +1,42 @@
 package com.inflearn.studyproject.account;
 
+import com.inflearn.studyproject.domain.Account;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
+
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
         model.addAttribute("signUpForm", new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+        return "redirect:/";
     }
 }
